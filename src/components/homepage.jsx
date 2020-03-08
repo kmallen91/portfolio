0702ce085d982a7075ef2aa685be703f998fcc7f
Kevin Allen <kevin.allen91@gmail.com>
2020-03-07 16:58:29 -0800

    update homepage info

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -7,8 +7,12 @@ export default function Homepage() {
     return (
         <div className='homepage-container'>
             <div className='description'>
-                Kevin Michael Allen <br/>
-                Software Engineer
+            <h1> Kevin Michael Allen </h1>
+             <h2> Software Engineer </h2>
+               <p> Full Stack Developer based in the San Francisco Bay Area. Strong foundations in
+                Javascript with an emphasis in React. Also proficient in Node/Express and am currently
+                learning Python with Django. Always looking to learn more and expand my horizons.
+                </p>
             </div>
             <div className='homepage-icons'>
                 <a href='https://www.linkedin.com/in/kevin-allen-9033b9190/'>
