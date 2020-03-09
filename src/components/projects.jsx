2100b9ec411173784353fe0802dd1a2393557ee3
Kevin Allen <kevin.allen91@gmail.com>
2020-03-08 17:19:01 -0700

    about and project styling update

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -10,7 +10,7 @@ export default function Projects() {
                 <a href='https://github.com/Lambda-School-Labs/forest-fire-watch-fe' className='project-link'> Github</a>
                 <br/>
                 <small>Capstone Project - Lambda School</small>
-                <p>
+                <p className='project-text'>
                 Web application development over two months with 3 other web developers and a data science team that shows the current wildfires within the United States and allows a logged in user to save locations and get text alerts if there is a wildfire within their given radius. Also allows users to toggle an air quality overlay to display the air quality information throughout the United States. <br/>
                 Responsible for creating front end components and back end databases to save and display data from users and the data science team. Also built the mapbox overlay to connect our data science team's air quality data to our application's map. <br/>
                 Stack:  React, Node, PostgreSQL, Jest, Heroku, Mapbox
@@ -22,7 +22,7 @@ export default function Projects() {
                 <a href='https://github.com/python-django-unchained' className='project-link'> Github</a>
                     <br/>
                     <small>Full Stack Collaboration</small>
-                    <p>
+                    <p className='project-text'>
                     Web game created in 4 days in collaboration with 3 other web developers to practice Python with Django. Players can select a planet to land on and explore, whlie discovering different sections throughout. <br/>
                     Responsible for creating front end components in order to display database algorithms and create a moveable player-character as well as unique environments for each planet. <br/>
                     Stack:  React, Python, Django, Canvas
@@ -34,7 +34,7 @@ export default function Projects() {
                     <a href='https://github.com/bw-ft-Dev-Libs/Front-End' className='project-link'> Github</a>
                         <br/>
                         <small>Front End Developer</small>
-                        <p>
+                        <p className='project-text'>
                         A web application that allows a user to create their own developer style mad-libs and update them on their profile page.                   
                         4 day project with 2 other front end developers and a back end developer. <br/>
                         Responsible for creating the front end interface to display the data created by users as well as those saved in the database. <br/>
