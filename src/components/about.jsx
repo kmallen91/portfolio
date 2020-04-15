c3a6beaeb43bfbc5f6e23dfc59eb8bcf43597e44
Kevin Allen <kevin.allen91@gmail.com>
2020-04-15 14:56:32 -0700

    updated about page with link to projects

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function About() {
 
@@ -8,9 +9,10 @@ export default function About() {
                 <h2 className='about-title'>About Me</h2>
                 <p className='about-text'> Former Chef who left working in Michelin rated restaurants to pursue Web Development. I've taken my hunger to learn about different cultures
                     and cuisines and applied them to creating amazing products through programming and web development. </p>
-                <p className='about-text'> I'm now a Full Stack Developer based in the San Francisco Bay Area. I have strong foundations in
+                <p className='about-text-2'> I'm now a Full Stack Developer based in the San Francisco Bay Area. I have strong foundations in
                 Javascript with an emphasis in React. Also proficient in Node/Express as well as HTML, CSS/SASS and Python with Flask. I'm always looking for the next challenge to learn more and expand my horizons.
                 </p>
+                <Link className='about-project-link' to='/projects'> Check out my most recent projects here! </Link>
             </div>
             
         </div>
