e9417996ebf8440c7f3045da18d67312667bab15
Kevin Allen <kevin.allen91@gmail.com>
2020-04-15 14:31:44 -0700

    added contact section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -12,13 +12,7 @@ export default function About() {
                 Javascript with an emphasis in React. Also proficient in Node/Express as well as HTML, CSS/SASS and Python with Flask. I'm always looking for the next challenge to learn more and expand my horizons.
                 </p>
             </div>
-            <div className='contact-section'>
-                <h2 className='contact-title'>Contact Me</h2>
-                <h3 className='email-title'>Email:</h3>
-                <a className='email-text' href ='mailto:kevin.allen91@gmail.com'>kevin.allen91@gmail.com</a>
-                <h3 className='email-title'>LinkedIn:</h3>
-                <a className='contact-text' href='https://www.linkedin.com/in/kevinmichaelallen/'>https://www.linkedin.com/in/kevinmichaelallen/</a>
-            </div>
+            
         </div>
     )
 }
--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -2,46 +2,15 @@ import React from 'react'
 
 
 const ContactForm = (props) => (
-  <form name = 'contact' className="contact-form" method='post'>
-
-    <input type="hidden" name="form-name" value="contact-form" />
-
-    <div className="form-field">
-      <label htmlFor="name">
-        <div className="label-content">Name:</div>
-        <input type="text" name="name" required />
-      </label>
+  <div className='contact-container'>
+    <div className='contact-section'>
+      <h2 className='contact-title'>Contact Me</h2>
+      <h3 className='email-title'>Email:</h3>
+      <a className='email-text' href ='mailto:kevin.allen91@gmail.com'>kevin.allen91@gmail.com</a>
+      <h3 className='email-title'>LinkedIn:</h3>
+      <a className='contact-text' href='https://www.linkedin.com/in/kevinmichaelallen/'>https://www.linkedin.com/in/kevinmichaelallen/</a>
     </div>
-
-    <div className="form-field">
-      <label htmlFor="email">
-        <div className="label-content">Email:</div>
-        <input type="email" name="email" required />
-      </label>
-    </div>
-
-    <div className="form-field">
-      <label htmlFor="message">
-        <div className="label-content">Message:</div>
-        <textarea className="stretch" name="message" rows="10" required />
-      </label>
-    </div>
-
-    <button className='form-button' type="submit" >Send</button>
-
-    <div>
-      { window.location.hash === '#success' &&
-        <div id="success">
-          <p>Your message has been sent!</p>
-        </div>
-      }
-      { window.location.hash === '#error' &&
-        <div id="error">
-          <p>An error occured while submitting the form.</p>
-        </div>
-      }
-    </div>
-  </form>
+  </div>
 )
 
 export default ContactForm
